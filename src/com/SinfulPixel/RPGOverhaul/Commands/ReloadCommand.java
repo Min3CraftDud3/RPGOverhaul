d16a2e0ddaeb99e06478c29b84be4985a5e365a8
Min3CraftDud3 <hubeb93@gmail.com>
2015-03-26 09:44:25 -0400

    Initial commit

---
+++ src/com/SinfulPixel/RPGOverhaul/Commands/ReloadCommand.java
@@ -0,0 +1,20 @@
+package com.SinfulPixel.RPGOverhaul.Commands;
+
+import com.SinfulPixel.RPGOverhaul.RPGOverhaul;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Created by Min3 on 3/26/2015.
+ */
+public class ReloadCommand implements CommandExecutor{
+    RPGOverhaul plugin;
+    public ReloadCommand(RPGOverhaul plugin){this.plugin=plugin;}
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        //Reload Plugin
+        return false;
+    }
+}
---
+++ src/com/SinfulPixel/RPGOverhaul/RPGOverhaul.java
@@ -0,0 +1,10 @@
+package com.SinfulPixel.RPGOverhaul;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Min3 on 3/26/2015.
+ */
+public class RPGOverhaul extends JavaPlugin {
+    public void onEnable(){}
+}
