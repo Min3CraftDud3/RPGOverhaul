06bbde210f1d5d5637c1e6cac3577d169f4ce797
Min3CraftDud3 <hubeb93@gmail.com>
2015-03-30 11:41:28 -0400

    Commit #13: Added Auto Respawning. Started work on player trading.

---
+++ src/com/SinfulPixel/RPGOverhaul/Listeners/PlayerTradeInitEvent.java
@@ -0,0 +1,38 @@
+package com.SinfulPixel.RPGOverhaul.Listeners;
+
+import com.SinfulPixel.RPGOverhaul.PlayerActions.PlayerTrade;
+import com.SinfulPixel.RPGOverhaul.RPGOverhaul;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+/**
+ * Created by Min3 on 3/30/2015.
+ */
+public class PlayerTradeInitEvent implements Listener {
+    RPGOverhaul plugin;
+    public PlayerTradeInitEvent(RPGOverhaul plugin){this.plugin=plugin;}
+    @EventHandler
+    public void onSheakClick(PlayerInteractEntityEvent e){
+        Player p = e.getPlayer();
+        if(p.isSneaking()){
+            if(e.getRightClicked() instanceof Player) {
+                Player target = (Player) e.getRightClicked();
+                if (PlayerTrade.tradeRequests.containsKey(target)) {
+                    PlayerTrade.openTradeGUI(p,target);
+                } else {
+                    PlayerTrade.tradeRequests.put(p, target);
+                    target.sendMessage(p.getName() + " wants to trade with you. Shift+Right-Click them to accept.");
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+                        @Override
+                        public void run() {
+                            PlayerTrade.tradeRequests.remove(p);
+                        }
+                    },20*60);
+                }
+            }
+        }
+    }
+}
---
+++ src/com/SinfulPixel/RPGOverhaul/PlayerActions/PlayerTrade.java
@@ -0,0 +1,17 @@
+package com.SinfulPixel.RPGOverhaul.PlayerActions;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+/**
+ * Created by Min3 on 3/30/2015.
+ */
+public class PlayerTrade {
+    public static HashMap<Player,Player> tradeRequests = new HashMap<Player,Player>(); //Player1 = Initiator , Player2 = Target
+
+    public static void openTradeGUI(Player p1, Player p2){
+        //Open Trade GUI
+    }
+
+}
